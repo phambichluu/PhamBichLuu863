777e1fdeb1d7c71b395a6008d079d8e526373c9e
DELL <DELL@DESKTOP-U4OBT9D>
2021-10-06 08:04:03 +0700

    Create_Table_PBL0863

--- PhamBichLuu863/Models/PersonPBL863.cs
+++ PhamBichLuu863/Models/PersonPBL863.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,11 @@ namespace PhamBichLuu863.Models
     public class PersonPBL863
     {
         [Key]
-       
+        [Column(TypeName = "varchar(20)")]
+
         public string PersonID { get; set; }
-       public string PersonName { get; set; }
+        [Column(TypeName = "varchar(50)")]
+
+        public string PersonName { get; set; }
     }
 }
